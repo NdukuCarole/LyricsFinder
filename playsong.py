eee74cdcd646901a853df740b8db79495683694d
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-08-07 09:03:03 +0300

    Update playsong.py

--- playsong.py
+++ playsong.py
@@ -1,13 +1,24 @@
 import urllib.request
 import urllib.parse
 import re
+import webbrowser
 
+#function for playing the track input
 
 def play_music(track_name):
  
+    #the request fro the url is made
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + track_name)
 
-    query_string = urllib.parse.urlencode({"search_query" : track_name})
-    html_content = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
-    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    print("https://www.youtube.com/watch?v=" + search_results)
+    #the track name(user input) is converted in to the youtube request  format for youtube
+    search_results = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    #the url is combined with the search results output above
+    video_link = "https://www.youtube.com/watch?v=" + search_results[0]
+
+    #the link is printed
+    print(video_link)
+    response='successful'
+    #web browser is prompted.
+    return webbrowser.open(video_link),response
+
    
